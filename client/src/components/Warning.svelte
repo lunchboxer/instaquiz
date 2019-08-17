bafe506efbd33519d97049bb95ac3ff18d040aa9
James <metal.lunchbox@gmail.com>
2019-08-18 02:10:35 +0800

    removing datepickr package

--- client/src/components/Warning.svelte
+++ client/src/components/Warning.svelte
@@ -4,9 +4,10 @@
 
 <style>
   .warning {
-    border: 1px solid #ffdd57;
     color: #ffdd57;
     margin-bottom: 1rem;
+    border-radius: 5px;
+    padding: 0;
   }
 
   .warning :global(strong) {
@@ -15,21 +16,22 @@
 
   .warning-message-header {
     margin: 0;
-    padding: 0.3rem 0.5rem;
-    background: #ffdd57;
-    color: black;
-    font-size: 1.2rem;
+    padding: 0.5rem 1rem;
+    color: #ffdd57;
+    background: black;
+    border-radius: 4px 4px 0 0;
+    border: 1px solid #ffdd57;
   }
 
   .warning-message-body {
-    padding: 0.3rem 0.6rem;
+    padding: 0.5rem 1rem;
   }
 </style>
 
 <div class="warning">
-  <p class="warning-message-header">
+  <h3 class="warning-message-header">
     {title}
-  </p>
+  </h3>
   <section class="warning-message-body">
     <slot></slot>
   </section>
