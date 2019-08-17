a05d5a313a1b86ebfd05e2b485fc61386459b1bc
James <metal.lunchbox@gmail.com>
2019-08-17 14:34:32 +0800

    fix build script

--- src/generated/prisma-client/index.js
+++ src/generated/prisma-client/index.js
@@ -40,6 +40,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `http://localhost:4467`
+  endpoint: `${process.env["ENDPOINT"]}`
 });
 exports.prisma = new exports.Prisma();
